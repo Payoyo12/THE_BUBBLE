e9a3a4f231555d6af3ed19cf995832ea921c8f9c
santiago ojeda arenas <clasedepoyo@gmail.com>
2022-11-23 13:06:55 +0100

    puntero

---
+++ Assets/scripts/Astilla.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Astilla : MonoBehaviour
+{
+    private Vector3 mousePos;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = Camera.main.transform.position.z + Camera.main.nearClipPlane;
+        transform.position = mousePos;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = Camera.main.transform.position.z + Camera.main.nearClipPlane;
+        mousePos.x += 1f;
+        mousePos.y += 0.7f;
+        transform.position = mousePos;
+        //transform.position = new Vector3(transform.position.x, transform.position.y, -1);
+    }
+}
