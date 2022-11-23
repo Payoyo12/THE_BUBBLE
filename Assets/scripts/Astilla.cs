f6243026dfc88bd2493957244692428ae8808ba7
santiago ojeda arenas <clasedepoyo@gmail.com>
2022-11-23 13:36:19 +0100

    puntero mejorado

--- Assets/scripts/Astilla.cs
+++ Assets/scripts/Astilla.cs
@@ -19,8 +19,8 @@ public class Astilla : MonoBehaviour
     {
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = Camera.main.transform.position.z + Camera.main.nearClipPlane;
-        mousePos.x += 1f;
-        mousePos.y += 0.7f;
+        mousePos.x += 0.8f;
+        mousePos.y += 0.5f;
         transform.position = mousePos;
         //transform.position = new Vector3(transform.position.x, transform.position.y, -1);
     }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # README #
-ss
+***ENLACES
+**musica sin copyright
+*https://www.youtube.com/watch?v=i46AjtQ1ghI
+
